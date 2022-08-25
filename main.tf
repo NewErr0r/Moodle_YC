daff79de9c0f874b38bd392cdb3e718afc68d7f0
”Dima <sechmma@gmail.com>
2022-08-25 09:58:20 +0300

    fixed main.tf

--- main.tf
+++ main.tf
@@ -14,51 +14,52 @@ provider "yandex" {
   zone      = "<зона доступности по умолчанию>"
 }
 
-data "yandex_compute_image" "centos_image" {
-  family   = "centos-stream-8"
+data "yandex_compute_image" "debian_image" {
+  family = "debian-11"
 }
 
-resource "yandex_compute_instance" "vm" {
-  name    = "vm"
-  zone    = "ru-central1-a"
+# Создаём VM 'vm'
+resource "yandex_compute_instance" "vm" {        
+    name        = "vm"  
+    zone        = "ru-central1-a"
 
-  resources {
-    cores   = 2
-    memory  = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.centos_image.id
-      size     = 20 
+    resources {
+        cores   = 2                                            
+        memory  = 2                                           
     }
-  }
 
-  network_interface {
-    subnet_id = "e9b9iosa2umis05ld5gu"    # default-ru-central1-a
-    nat       = true 
-  }
+    boot_disk {
+        initialize_params {
+            image_id = data.yandex_compute_image.debian_image.id
+            size     = 15 #GB
+        }
+    }
 
-  scheduling_policy {
-    preemptible = true
-  }
+    network_interface {
+        subnet_id   = "e9b9iosa2umis05ld5gu"    # default-ru-central1-a
+        nat         = true
+    }
 
-  metadata = {
-    startup-script = "${file("./script.sh")}"
-    ssh-key        = "err0r:${file("./keys/id_rsa.pub")}"
-  }
+    scheduling_policy {
+      preemptible = true                        # Прерываемая VM            
+    }
+
+    metadata = {
+        ssh-keys = "debian:${file("./keys/id_rsa.pub")}"     # debian - логин пользователя
+    }
 }
 
+# Создание динамического inventory для Ansible для подключения к APP_GW
 data "template_file" "inventory" {
-  template  = file("./_templates/inventory.tpl")  
-
-  vars = {
-    user = "err0r"
-    host = join("", [yandex_compute_instance.vm.name, " ansible_host=", yandex_compute_instance.vm.network_interface.0.nat_ip_address])
-  }
+    template = file("./_templates/inventory.tpl")
+  
+    vars = {
+        user = "debian"
+        host = join("", [yandex_compute_instance.vm.name, " ansible_host=", yandex_compute_instance.vm.network_interface.0.nat_ip_address])
+    }
 }
 
 resource "local_file" "save_inventory" {
-  content  = data.template_file.inventory.rendered
-  filename = "./inventory"  
+   content  = data.template_file.inventory.rendered
+   filename = "./inventory"
 }
--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dnf install -y epel-release
-dnf install -y python3 firewalld
-systemctl enable --now firewalld
